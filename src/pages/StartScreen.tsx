bf447787f3d7de24ff766ca2dc15ef698940a0ea
sung-silver <jinna0319@gmail.com>
2026-04-02 00:26:37 +0900

    refactor: 코드래빗 코드 리뷰 반영

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { Button } from '@sopt-makers/ui';
 import { fontsObject } from '@sopt-makers/fonts';
 import { colors } from '@sopt-makers/colors';
@@ -6,17 +5,15 @@ import styles from './StartScreen.module.css';
 import backgroundImg from '../assets/background.png';
 import titleImg from '../assets/title.png';
 
-// 하드코딩된 참여 기간 설정
+// 하드코딩된 참여 기간 설정 (추후 Supabase에서 불러올 예정)
 const PARTICIPATION_START_DATE = new Date('2026-04-01T00:00:00');
 const PARTICIPATION_END_DATE = new Date('2026-04-30T23:59:59');
 
-function StartScreen() {
-  // 현재 날짜가 참여 기간 내인지 체크
-  const isActive = useMemo(() => {
-    const now = new Date();
-    return now >= PARTICIPATION_START_DATE && now <= PARTICIPATION_END_DATE;
-  }, []);
+// 현재 날짜가 참여 기간 내인지 체크
+const now = new Date();
+const isActive = now >= PARTICIPATION_START_DATE && now <= PARTICIPATION_END_DATE;
 
+function StartScreen() {
   return (
     <div className={styles.container}>
       {/* Background Image */}
@@ -24,9 +21,7 @@ function StartScreen() {
 
       {/* Main Content */}
       <div className={styles.mainContent}>
-        <div className={styles.titleGroup}>
-          <img src={titleImg} className={styles.titleImage} />
-        </div>
+        <img src={titleImg} alt="너의 목소리가 들려" className={styles.titleImage} />
 
         {isActive && (
           <Button
@@ -35,6 +30,7 @@ function StartScreen() {
             theme="white"
             variant="fill"
             style={fontsObject.LABEL_1_18_SB}
+            // TODO: 안내 페이지로 이동
           >
             너목들 시작하기
           </Button>
