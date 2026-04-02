c4311c0b4e7fe5de5633635bbc912e46961ca074
sung-silver <jinna0319@gmail.com>
2026-04-02 19:44:29 +0900

    feat: 줄바꿈 태그 변경

--- src/pages/NoticeScreen.tsx
+++ src/pages/NoticeScreen.tsx
@@ -20,17 +20,18 @@ function NoticeScreen() {
 
         <div className={styles.descriptionSection} style={{ ...fontsObject.BODY_3_14_M, color: colors.white }}>
           <p>
-          너의 목소리가 들려에서 작성하는 모든 코멘트는 무기명으로 이루어져요.
+            너의 목소리가 들려에서 작성하는 모든 코멘트는 무기명으로 이루어져요.
           </p>
           <p className={styles.indentSection}>
             <li>Start 및 Continue 코멘트는 makers 노션의 '너의 목소리가 들려 DB'에서 확인할 수 있어요.</li>
             <li>Stop 코멘트는 '무무봇'이 Slack 개인 디엠으로 전달해드려요.</li>
           </p>
           <p>
-          또한 모든 코멘트에 대해 작성하실 필요는 없어요.<br></br>Stop, Start, Continue 중 필요한 코멘트만 골라 남겨주시면 돼요.
+            또한 모든 코멘트에 대해 작성하실 필요는 없어요.<br />
+            Stop, Start, Continue 중 필요한 코멘트만 골라 남겨주시면 돼요.
           </p>
           <p>
-          위의 내용에 대해 숙지하셨다면, 다음으로 넘어가 주세요. 🔥
+            위의 내용에 대해 숙지하셨다면, 다음으로 넘어가 주세요. 🔥
           </p>
         </div>
 
