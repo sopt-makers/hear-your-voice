790a2e9c663530a8a834a1f7c188b51abbdd0e78
sung-silver <jinna0319@gmail.com>
2026-04-09 00:19:04 +0900

    feat: 스프린트 코드 입력 시 스프린트 안내 페이지 연계(스프린트 이름 동적 변경) 구현

--- src/pages/SprintIntroPage.tsx
+++ src/pages/SprintIntroPage.tsx
@@ -1,10 +1,18 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import { StepLayout } from '../components';
 import * as styles from './SprintIntroPage.css';
 import sprintCodeVerifiedImg from '../assets/sprint_code_verified.png';
+import type { SprintType } from '../types/sprint';
 
 function SprintIntroPage() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const sprintName: string = location.state?.sprintName ?? '챕터별 스프린트';
+  const sprintType: SprintType = location.state?.sprintType ?? 'chapter';
+
   const handleStart = () => {
     // TODO: 다음 온보딩/챕터 화면으로 이동 (예: navigate('/chapter-…'))
+    navigate('/next', { state: { sprintType } });
   };
 
   return (
@@ -19,7 +27,7 @@ function SprintIntroPage() {
       <div className={styles.content}>
         <img src={sprintCodeVerifiedImg} alt="" className={styles.characterImage} />
         <p className={styles.introLine}>
-          챕터별 스프린트에 대한
+          {sprintName}에 대한
           <br /> 너목들을 시작할까요?
         </p>
       </div>
