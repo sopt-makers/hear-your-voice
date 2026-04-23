bb1bb056328bcd9f76e58cb6f20934cec853260e
sung-silver <jinna0319@gmail.com>
2026-04-23 13:16:48 +0900

    feat: 에러 페이지 layout 설정

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,14 @@
+import { StepLayout } from '@components';
+
 function ErrorPage() {
   return (
-    <div>
-      <p>오류가 발생했어요. 잠시 후 다시 시도해주세요.</p>
-    </div>
+    <StepLayout showProgressBar={false}>
+      <div style={{ textAlign: 'center', padding: '2rem' }}>
+        <h1>오류가 발생했습니다.</h1>
+        <p>죄송합니다. 페이지를 불러오는 중에 문제가 발생했습니다.</p>
+        <p>잠시 후 다시 시도해 주세요.</p>
+      </div>
+    </StepLayout>
   );
 }
 
